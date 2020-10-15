ab93c7c6777389368284ae9d8d690bca9d2b1484
nancyruya <nancyruya@gmail.com>
2020-10-14 20:28:15 -0400

    Axios is like Ajax: can make requests to post/get from servers

--- frontend/gui/src/components/Blog.js
+++ frontend/gui/src/components/Blog.js
@@ -2,19 +2,6 @@ import React from 'react';
 import { List, Avatar, Space } from 'antd';
 import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
 
-const listData = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://ant.design',
-    title: `ant design part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
 const IconText = ({ icon, text }) => (
   <Space>
     {React.createElement(icon)}
@@ -22,7 +9,7 @@ const IconText = ({ icon, text }) => (
   </Space>
 );
 
-const Blog =(props) => {
+const Blogs =(props) => {
     return (
   <List
     itemLayout="vertical"
@@ -33,7 +20,7 @@ const Blog =(props) => {
       },
       pageSize: 3,
     }}
-    dataSource={listData}
+    dataSource={props.data}
     footer={
       <div>
         <b>ant design</b> footer part
@@ -67,4 +54,4 @@ const Blog =(props) => {
     )
 }
 
-export default Blog; 
+export default Blogs; 
